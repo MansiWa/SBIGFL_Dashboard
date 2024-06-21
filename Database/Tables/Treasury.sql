4e96457ef1b85ea6ee67934640dfa18d03397719
MansiWa <94973642+MansiWa@users.noreply.github.com>
2024-06-21 17:41:22 +0530

    database changes

---
+++ Database/Tables/Treasury.sql
@@ -0,0 +1,16 @@
+alter table tbl_Borrowing
+add FileDate varchar(50) null
+alter table tbl_TIER_II_BOND
+add FileDate varchar(50) null
+alter table tbl_Commercial_Paper
+add FileDate varchar(50) null
+alter table tbl_Bank_Line_CC
+add FileDate varchar(50) null
+alter table tbl_Bank_Line_WCL
+add FileDate varchar(50) null
+alter table tbl_FOREX
+add FileDate varchar(50) null
+alter table tbl_RFRDoc
+add FileDate varchar(50) null
+alter table tbl_BorroAndRFR
+alter column rb_date varchar(50) null
